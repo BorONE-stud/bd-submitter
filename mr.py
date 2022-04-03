71b5362239089f6d599f042220740c5d2d3749d8
Saveliy Borodin <b1@Saveliys-MacBook-Air.local>
2022-04-03 20:52:13 +0300

    forgotten

---
+++ mr.py
@@ -0,0 +1,88 @@
+import gitlab
+from sys import argv
+
+name = 'borodinsa'
+
+class color:
+  attention = "\033[033m"
+  reset = "\033[0m"
+
+def print_help():
+  print("Usage: python3 mr [task]...")
+  print()
+  print("Creates merge request and attaches screenshot.")
+  print("Screenshot should be in a directory named after homework e.g. 'hw3'.")
+  print("Screenshot should be named after task on site e.g. '7.png'.")
+  print()
+  print(color.attention, "Set 'name'!!!", color.reset, sep='')
+  print("Check top of the file")
+  print()
+  print(color.attention, "Set private token!!!", color.reset, sep='')
+  print("Go to")
+  print(" gitlab > Edit profile > Access token")
+  print("tick api and save the token to token-file")
+
+
+def token(token_path: str = 'token'):
+  # edit profile > acces token > api
+  with open(token_path, 'r') as file:
+    result = file.readline()[:-1]
+  return result
+
+def map_task(task):
+  tasks = {
+    'hw1': [ 1,  2, 10, 17, 31,  33,  38,  44, 53, 62],
+    'hw2': [11, 12, 15, 20, 22,  29,  30,  68, 74, 86],
+    'hw3': [ 7,  9, 14, 16, 23,  32,  48,  84],
+    'hw4': [37, 41, 47, 70, 73,  87, 102, 112],
+    'hw5': [65, 82, 90, 96, 98, 100, 105, 109],
+  }
+  for hw, hw_tasks in tasks.items():
+    try:
+      ord_task = hw_tasks.index(task) + 1
+      return {
+        'hw': hw,
+        'ord_task': f'task{ord_task}',
+        'site_task': str(task)
+      }
+    except:
+      pass
+  raise ValueError(f'task {task} not found')
+
+def create_mr(project, *, hw, ord_task, site_task):
+  """
+  Creates MR, attaches screenshot.
+  Args:
+    hw: string e.g. 'hw3'
+    ord_task: string e.g. 'task2'
+    site_task: string e.g. '9'
+  Returns:
+    created MR
+  """
+  uploaded = project.upload(site_task, filepath=f'{hw}/{site_task}.png')
+
+  return project.mergerequests.create({
+    'source_branch': f'{hw}{ord_task}',
+    'target_branch': 'main',
+    'title': f'{hw}{ord_task}',
+    'description': f"!{uploaded['markdown']}"
+  })
+
+def get_project_by(*, name):
+  # this is kinda naive, but whatever
+  gl = gitlab.Gitlab('https://gitlab2.atp-fivt.org', private_token=token())
+  return next(project for project in gl.projects.list() if project.name == name)
+
+def main():
+  if len(argv) == 1 or argv[1] == '--help':
+    print_help()
+    return
+
+  for task in argv[1:]:
+    mapped = map_task(int(task))
+    project = get_project_by(name=f"{name}-{mapped['hw']}")
+    mr = create_mr(project, **mapped)
+    print(f"{mapped['hw']}{mapped['ord_task']}:", mr.web_url)
+
+if __name__  == '__main__':
+  main()
